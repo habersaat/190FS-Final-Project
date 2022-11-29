688efcd281cc9e5b05ed4f06b8164091d3c5ce5a
Hunter Habersaat <hah50@duke.edu>
2022-11-29 18:28:01 -0500

    Correct version of Unity

--- Assets/WorldJump.cs
+++ Assets/WorldJump.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+ 
 public class WorldJump : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Upon collision with another GameObject, this GameObject will reverse direction
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.name == "VRHeadset") {
+            SceneManager.LoadScene(1);
+            Debug.Log("Working...");
+        } 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
