a69648379ce11efe0a5df22b2b7d9e0e0fcc2615
Hunter Habersaat <hah50@duke.edu>
2022-11-29 20:35:42 -0500

    Fade transition between scenes

---
+++ Assets/FadeScreen.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeScreen : MonoBehaviour
+{
+    public float duration = 2;
+    public Color fadeColor = Color.white;
+    private Renderer rend;
+    public bool fadeOnStart = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        if(fadeOnStart) FadeIn();
+    }
+
+    public IEnumerator FadeRoutine(float opIn, float opOut) {
+        float timer = 0;
+        while(timer <= duration) {
+            Color update = fadeColor;
+            update.a = Mathf.Lerp(opIn, opOut, timer / duration);
+            rend.material.SetColor("_Color", update);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        Color final = fadeColor;
+        final.a = opOut;
+        rend.material.SetColor("_Color", final);
+    }
+
+    public void Fade(float opIn, float opOut) {
+        StartCoroutine(FadeRoutine(opIn, opOut));
+    }
+
+    public void FadeIn() {
+        Fade(1,0);
+    }
+
+    public void FadeOut() {
+        Fade(0,1);
+    }
+}
--- Assets/WorldJump.cs
+++ Assets/WorldJump.cs
@@ -5,12 +5,25 @@ using UnityEngine.SceneManagement;
  
 public class WorldJump : MonoBehaviour
 {
+    public FadeScreen fadeScreen;
+
+    public void GoToScene(int idx) {
+        GoToSceneRoutine(idx);
+    }
+
+    IEnumerator GoToSceneRoutine(int idx) {
+        fadeScreen.FadeOut();
+        yield return new WaitForSeconds(fadeScreen.duration);
+
+        SceneManager.LoadScene(idx);
+    }
+
     //Upon collision with another GameObject, this GameObject will reverse direction
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "VRHeadset") {
             int x = SceneManager.GetActiveScene().buildIndex;
-            SceneManager.LoadScene(x+1);
+            GoToScene(x+1);
             Debug.Log("Working...");
         } 
     }
